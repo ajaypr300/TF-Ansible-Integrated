83dfc182c5361cccc15f2d547c513c3a17a68115
vijayprabhu04 <49646334+vijayprabhu04@users.noreply.github.com>
2021-05-10 11:42:29 +0530

    Add files via upload

---
+++ TF-code/ec2_creation/vars.tf
@@ -0,0 +1,17 @@
+# Region
+variable AWS_REGION {
+    default  = "ap-south-1"
+}
+
+# Instance
+variable ami_id {
+    default = "ami-0a9d27a9f4f5c0efc"
+}
+
+variable instance_type {
+    default = "t2.micro"
+}
+
+variable sg {
+    default = "sg-0a254ce4223288a2a"
+}
